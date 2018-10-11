c6673a4bb6d4c1c5f537dfef61088fda2e2be800
gucao <280780363@qq.com>
2018-10-11 17:37:44 +0800

    ...

--- src/components/FormDesiner.vue
+++ src/components/FormDesiner.vue
@@ -1,18 +1,53 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-edit"></el-button>
-    <el-button type="primary" icon="el-icon-share"></el-button>
-    <el-button type="primary" icon="el-icon-delete"></el-button>
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
-    <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+    <table>
+      <thead>
+        <th>empty</th>
+        <th></th>
+      </thead>
+      <tr></tr>
+    </table>
   </div>
 </template>
 <script>
 export default {
+  components: {},
   data() {
-    return {};
+    return {
+      enums: {},
+      tempData: {
+        table: {
+          colCount: 8,
+          rowCount: 8
+        }
+      },
+      cellData: [
+        {
+          row: 1,
+          col: 1,
+          content: {
+            type: "text",
+            value: "aaaa"
+          }
+        }
+      ]
+    };
   },
-  components: {}
+  methods: {
+    addCol() {},
+    addRow() {},
+    delCol() {},
+    delRow() {},
+    mergeCell() {},
+    resizeColWidth() {},
+    resizeRowHeight() {},
+    getColHeaderTitle(colIndex) {
+      let last = String.fromCharCode((colIndex % 26 || 26) + 64);
+      let i = Math.floor((colIndex - 1) / 26);
+      let first = i == 0 ? "" : String.fromCharCode(i + 64);
+      return first + last;
+    }
+  }
 };
 </script>
 <style lang="less" scoped>
