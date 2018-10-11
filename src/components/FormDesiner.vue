134d2edcdc40fc7a7cd2c407489c6580f916463b
gucao <280780363@qq.com>
2018-10-11 11:08:34 +0800

    ...

--- src/components/FormDesiner.vue
+++ src/components/FormDesiner.vue
@@ -1,25 +1,19 @@
 <template>
-    <div>
-        <el-button type="primary" icon="el-icon-edit"></el-button>
-        <el-button type="primary" icon="el-icon-share"></el-button>
-        <el-button type="primary" icon="el-icon-delete"></el-button>
-        <el-button type="primary" icon="el-icon-search">搜索</el-button>
-        <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
-    </div>
+  <div>
+    <el-button type="primary" icon="el-icon-edit"></el-button>
+    <el-button type="primary" icon="el-icon-share"></el-button>
+    <el-button type="primary" icon="el-icon-delete"></el-button>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+  </div>
 </template>
 <script>
-
 export default {
-    data() {
-        return {
-          
-        };
-    },
-    components: {
-    },
+  data() {
+    return {};
+  },
+  components: {}
 };
 </script>
 <style lang="less" scoped>
 </style>
-
-
